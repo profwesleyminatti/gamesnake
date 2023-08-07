5b34f4c45058f4b9c980082fd58c1b1aa5bd97b2
profwesleyminatti <108468583+profwesleyminatti@users.noreply.github.com>
2023-08-07 15:01:37 +0000

    Commit js

---
+++ game.js
@@ -0,0 +1,49 @@
+const playBoard =
+    document.querySelector(".play-board");
+const scoreElement =
+    document.querySelector(".score");
+const highScoreElement =
+    document.querySelector(".high-score");
+const controls =
+    document.querySelectorAll(".controls i");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = 'High Score: ${highscore}';
+
+const updateFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const handGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over!!!!");
+    location.reload();
+}
+
+const changeDirection = e => {
+
+    if (e.key == "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key == "ArrowDown" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key == "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key == "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+
+}
+
